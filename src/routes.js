3926dc6d19834128de85cec06a15ae24b39b1b24
randel <randelfreitas@gmail.com>
2020-08-11 16:28:46 -0300

    Adicionando autenticaçãoi

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import Adm from './pages/Client/Adm';
 
 import Site from './Site';
 import App from './pages/Client/User';
 import Suggest from './pages/Suggest';
+
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+      )
+    }
+  />
+);
  
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Site}/>
+      <Route exact path='/login' component={()=><h1>Login</h1>}/>
       <Route path='/app' component={App}/>
-      <Route path='/adm' component={Adm}/>
-      <Route path='/suggest' component={Suggest}/>
+      <PrivateRoute path='/adm' component={Adm}/>
+      <PrivateRoute path='/suggest' component={Suggest}/>
     </Switch>
   </BrowserRouter>
 );
---
+++ src/services/auth.js
@@ -0,0 +1,19 @@
+export const TOKEN_KEY = "@token";
+export const NAME = "@name";
+
+export const isAuthenticated = () => false;//localStorage.getItem(TOKEN_KEY) !== null;
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const getName = () => localStorage.getItem(NAME);
+
+export const setInfosLocalStorage = (token, name) => {
+  localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(NAME, name);
+};
+export const setTokenLocalStorage = (token) => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+export const logoutApp = () => {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(NAME);
+};
