85d45a852152c5998fd871983e2d47d36a841e7f
randel <randelfreitas@gmail.com>
2020-08-25 22:16:56 -0300

    Inserindo material ui

--- src/pages/Client/FormClient/index.js
+++ src/pages/Client/FormClient/index.js
@@ -1,93 +1,165 @@
-import React, { useState, useEffect} from 'react';
-import { bindActionCreators } from 'redux';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { submitSuggest } from '../../../store/clientReducer'
 import { connect } from 'react-redux';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { submitSuggest } from '../../../store/clientReducer';
+import NumberFormat from 'react-number-format';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Checkbox from '@material-ui/core/Checkbox';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Star from '@material-ui/icons/Star';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-import styles from './index.module.css';
-
-const FormClient = (props) => {
-  const { infos } = props;
-  const [ disab, setDisab] = useState(false);
-  
-  const desabilitar =() =>{
-    if(disab === false){
-      setDisab(true);
-    }else{
-      setDisab(false);
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: 'auto',
+      width: '33ch',
+    },
+  },
+  center: {
+    width: '275px',
+    margin: 'auto',
+    paddingTop: '30px'
+  },
+  span: {
+    marginTop: '20px',
+    fontSize: "15px",
+    textAlign: 'center',
+    alignItems: 'center'
+  },
+  desejo: {
+    fontSize: "12px",
+  },
+  desejoPai: {
+    margin: 0,
+    justifyContent: 'start'
+  },
+  button: {
+    marginTop: '20px'
   }
+}));
 
-  return(
-    <Formik
-      initialValues={{
-        name: '', 
-        phone: '',
-        note: '',
-        recomends: '',
-        opinion: '',
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1769aa',
+    },
+    secondary:{
+      main: '#FFB701'
+    }
+  },
+});
+
+const NumberFormatCustom = (props) => {
+  const { inputRef, onChange, ...other } = props;
+  return (
+    <NumberFormat
+      {...other}
+      getInputRef={inputRef}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
       }}
-      validationSchema={Yup.object({
-        note: Yup.string()
-        .required('Que nota nós merecemos?'),
-        recomends: Yup.string()
-        .required('Nos recomendaria?'),
-        opinion: Yup.string()
-        .required('Alguma sugestão?'),
-      })}
-      onSubmit={(values) => {
-        props.submitSuggest(values, infos._id);
-      }}
-    >
-      <Form>
-        <div className={styles.wrap}>
-          <label className={styles.label}>Nome:</label>
-          <Field className={styles.input} disabled={disab} name='name' type='text' maxLength='50'></Field>
-        </div>
-        <div className={styles.wrap}>
-          <label className={styles.label}>Telefone:</label>
-          <Field className={styles.input} disabled={disab} name='phone' type='text' maxLength='11'/>
-          <Field name='anonimus' type='checkbox' id='desab' onClick={()=>desabilitar()}/>
-          <label className={styles.labelCheckbox} htmlFor='desab'>Desejo não me identificar</label>
-        </div>
-        <div className={styles.wrap}>
-          <p className={styles.label}>Nota ao estabelecimento:*</p>
-          <div className={styles.center}>
-            <Field id='1' type='radio' name='note' value='1'/>
-            <label className={styles.labelRadio} htmlFor='1'>1</label>
-            <Field id='2' type='radio' name='note' value='2'/>
-            <label className={styles.labelRadio} htmlFor='2'>2</label>
-            <Field id='3' type='radio' name='note' value='3'/>
-            <label className={styles.labelRadio} htmlFor='3'>3</label>
-            <Field id='4' type='radio' name='note' value='4'/>
-            <label className={styles.labelRadio} htmlFor='4'>4</label>
-            <Field id='5' type='radio' name='note' value='5'/>
-            <label className={styles.labelRadio} htmlFor='5'>5</label>
-          </div>
-          <p className={styles.erro}><ErrorMessage name='note'/></p>
-        </div>
-        <div className={styles.wrap}>
-          <label className={styles.label}>Nos recomendaria a um amigo?*</label>
-          <div className={styles.center}>
-            <Field id='sim' type='radio' name='recomends' value='Y'/>
-            <label className={styles.labelRadio} htmlFor='sim'>Sim</label>
-            <Field id='nao' type='radio' name='recomends' value='N'/>
-            <label className={styles.labelRadio} htmlFor='nao'>Não</label>
-          </div>
-          <p className={styles.erro}><ErrorMessage name='recomends'/></p>
-        </div>
-        <div className={styles.wrap}>
-          <label className={styles.label}> Deixe sua opinião:*</label>
-          <Field className={styles.textArea} name='opinion' component='textarea' rows='5' cols='34' maxLength='300'/>
-          <p className={styles.erro}><ErrorMessage name='opinion'/></p>
-        </div>
-        <div>
-          <button className={styles.link} type='submit'>Enviar opinião</button>
-        </div>
-      </Form>
-    </Formik>
+      isNumericString
+      format='(##) #########'
+    />
+  );
+}
+
+NumberFormatCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const FormClient = () => {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  const [values, setValues] = useState({
+    name: '',
+    phone: '',
+    stars: '',
+    opinion: ''
+  });
+
+  const handleChange = (event) => {
+    setValues({...values, [event.target.name]: event.target.value });
+    console.log(values);
+  };
+  const checkChange = (event) => {
+    setValues({name: '', phone: ''});
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <div className={classes.center}>
+      <MuiThemeProvider theme={theme}>
+        <FormControl className={classes.root}>
+          <TextField
+            label="Nome"
+            error
+            disabled={checked}
+            value={values.name} 
+            onChange={handleChange}
+            name="name"
+            id="standard-basic" 
+          />
+          <TextField
+            label="Telefone"
+            error
+            disabled={checked}
+            value={values.phone}
+            onChange={handleChange}
+            name="phone"
+            id="formatted-numberformat-input"
+            InputProps={{inputComponent: NumberFormatCustom}}
+          />
+          <FormControlLabel className={classes.desejoPai}
+            label={<Typography className={classes.desejo}>Desejo não me identificar</Typography>}
+            size="small"
+            control={<Checkbox color="primary" checked={values.checkedA} 
+            onChange={checkChange} 
+            name="checkedA" />}
+          />
+          <Box className={classes.span} component="fieldset" mb={3} borderColor="transparent">
+            <Typography component="legend">Toque para classificar</Typography>
+            <Rating
+              name="stars"
+              onChange={handleChange}
+              defaultValue={0}
+              icon={<Star fontSize="large" />}
+              emptyIcon={<StarBorderIcon fontSize="large" />}
+            />
+          </Box>
+          <TextField error
+            label="Deixe sua opinião"
+            multiline
+            rows={4}
+            rowsMax={6}
+            variant="outlined"
+            value={values.opinion} 
+            onChange={handleChange}
+            name="opinion"
+            id="text-basic" 
+          />
+          <Button className={classes.button} variant="contained" color="primary">
+            Enviar
+          </Button>
+        </FormControl>
+      </MuiThemeProvider>
+    </div>
   );
 }
 
--- src/pages/Client/index.js
+++ src/pages/Client/index.js
@@ -5,12 +5,75 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import RoutesClient from './routesClient';
 import { getInfo } from '../../store/clientReducer';
-import styles from './index.module.css';
 import history from '../../services/history';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles((theme) => ({
+  header:{
+    background: '#FFB701',
+    textAlign: 'center',
+  },
+  img:{
+    padding: '10px'
+  },
+  menu: {
+    height: '25px'
+  },
+  links:{
+    margin: 'auto'
+  },
+  labelLink:{
+    padding: '10px',
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 220,
+  },
+  center: {
+    textAlign: 'center',
+    justifyContent: 'center'
+  },
+  offset: theme.mixins.toolbar,
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FED200',
+    },
+    secondary:{
+      main: '#FFB701'
+    }
+  },
+});
 
 const Client = (props) => {
   const { infos } = props;
   const [ idUrl ] = useState(window.location.href.split('/?')[1]);
+  const [state, setState] = React.useState(false);
+  const classes = useStyles();
 
   useEffect(() => {
     if(idUrl){
@@ -20,34 +83,56 @@ const Client = (props) => {
     }
   },[]);
 
+  const toggleDrawer = (open) => (event) => {
+    setState(open);
+  };
+
+  const list = (
+    <div className={classes.list} onClick={toggleDrawer(false)} role="presentation" onKeyDown={toggleDrawer(false)}>
+      <List>
+        <ListItem button>
+          <Button component={Link} to={`/client/?${infos._id}`}>
+            <ListItemIcon><InboxIcon/></ListItemIcon>
+            <ListItemText primary='Início' />
+          </Button>
+        </ListItem>
+        <Divider/>
+        <ListItem button>
+          <Button component={Link} to={`/client/sobrenos/?${infos._id}`}>
+            <ListItemIcon><InboxIcon/></ListItemIcon>
+            <ListItemText primary='Sobre nós' />
+          </Button>
+        </ListItem>
+        <Divider/>
+      </List>
+    </div>
+  )
   return(
-    <div>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <div>
-            <img src='/assets/logo_palpitebox.png' alt='logo'/>
-          </div>
-        </div>
-        <nav>
-          <div>
-            <ul className={styles.menu}>
-              <li><Link to={`/client/?${infos._id}`} className={styles.link}>INÍCIO</Link></li>
-              <li><Link to={`/client/sobrenos/?${infos._id}`} className={styles.link}>SOBRE</Link></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <div className={styles.section}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar color="primary">
+          <IconButton onClick={toggleDrawer(true)} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Drawer open={state} onClose={toggleDrawer(false)}>
+            {list}
+          </Drawer>
+          <Typography variant="button" className={classes.links}>
+            <Button component={Link} to={`/client/?${infos._id}`}>SuggestInBox</Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div>
         <RoutesClient/>
       </div>
-      <footer className={styles.footer}>
-        <div>
-          <p>Colocar Suggest na sua empresa, <Link to='/'>clique aqui!</Link></p>
-          <p>Suggest In Box - Todos os direitos reservados.</p>
-          <p>© 2020 - Copyright</p>
-        </div>
-      </footer>
-    </div>
+      <AppBar position="fixed" color="secondary" className={classes.appBar}>
+        <Toolbar className={classes.center}>
+          <Typography variant="caption" >SuggestInBox © - 2020 Copyright.</Typography>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
+    </MuiThemeProvider>
   );
 }
 
